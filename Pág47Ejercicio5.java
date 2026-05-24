d23a0a739edb9303d24ff26abd0df644ebb847ec
lilishlit <m19-0145@ipl.edu.do>
2026-05-24 14:19:26 -0400

    subiendo el ejercicio 5

---
+++ Pág47Ejercicio5.java
@@ -0,0 +1,15 @@
+//Hecho por: Brashly Cast. (2024-0014)
+public class Pág47Ejercicio5 {
+    public static void main(String[] args) {
+        float numero = 23.84f;
+        
+        // Convierto el float a string
+        String nomb = String.valueOf(numero);
+        
+        // Comprobación
+        System.out.println("El valor de nomb es: " + nomb);
+        
+        // Prueba para demostrar que ya es texto
+        System.out.println("Prueba textual de suma (nomb + 10)= " + (nomb + 10)); 
+    }
+}
