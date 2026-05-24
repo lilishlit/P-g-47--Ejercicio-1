44fb5ae40582f74f8348fbffbf03cd8b6118a899
lilishlit <m19-0145@ipl.edu.do>
2026-05-24 14:00:58 -0400

    subir ejercicio 3
    This Java program converts a string to an integer and performs basic arithmetic operations.

---
+++ Pág47Ejercicio3.java
@@ -0,0 +1,15 @@
+//Hecho por: Brashly Cast. (2024-0014)
+public class Pág47Ejercicio3 {
+    public static void main(String[] args) {
+        String cadena = "200";
+        
+        // Conversión a entero en la variable E1
+        int E1 = Integer.parseInt(cadena);
+        System.out.println("El valor de E1 es= " + E1);
+
+        // Comprobamos
+        System.out.println("Prueba de suma (E1 + 15)= " + (E1 + 15));
+        System.out.println("Prueba de resta (E1 - 10)= " + (E1 - 10));
+        System.out.println("Prueba con multiplicación (E1 * 3)= " + (E1 * 3));
+    }
+}
