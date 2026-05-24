d550157447443d2f4f2d807f59b6fae685df2a7a
lilishlit <m19-0145@ipl.edu.do>
2026-05-24 13:57:02 -0400

    subir segundo ejercicio

--- Pág47Ejercicio2.java
+++ Pág47Ejercicio2.java
@@ -1,1 +1,15 @@
-
+public class Pág47Ejercicio2 {
+    public static void main(String[] args) {
+        // Cadena
+        String cadena = "200";
+        
+        // Conversión en la variable E
+        int E = Integer.parseInt(cadena);
+        System.out.println("El valor de E es= " + E);
+        
+        // Comprobamos
+        System.out.println("Prueba de suma (E + 5): " + (E + 5));
+        System.out.println("Prueba de resta (E - 5): " + (E - 5));
+        System.out.println("Prueba con multiplicación (E * 5): " + (E * 5));
+        }
+}
