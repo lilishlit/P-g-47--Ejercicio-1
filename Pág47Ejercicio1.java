3d8806ca5805aa0dc4f476b423193298171af193
Brashly Cast. <m19-0145@ipl.edu.do>
2026-05-24 11:12:11 -0400

    lol

---
+++ Pág47Ejercicio1.java
@@ -0,0 +1,43 @@
+class DeclaracionDatos {
+    public static void main(String[] args) {
+
+        // Entero a
+        int a = 0;
+
+        // Entero x, y
+        int x = 10, y = 20;
+
+        // Double x
+        double x1 = 5.5;
+
+        // Double x, y
+        double x2 = 7.8, y2 = 9.3;
+
+        // Entero a, b, c con valores iniciales 5, 6, 7
+        int a2 = 5, b = 6, c = 7;
+
+        // Lógico sw con valor inicial falso
+        boolean sw = false;
+
+        // Cadena cad con valor nulo
+        String cad = null;
+
+        // Constante PI con valor 3.14
+        final double PI = 3.14;
+
+        // Imprimir valores
+        System.out.println("Valor de a: " + a);
+        System.out.println("Valores de x e y: " + x + ", " + y);
+
+        System.out.println("Valor de x1: " + x1);
+        System.out.println("Valores de x2 e y2: " + x2 + ", " + y2);
+
+        System.out.println("Valores de a2, b y c: " + a2 + ", " + b + ", " + c);
+
+        System.out.println("Valor de sw: " + sw);
+
+        System.out.println("Valor de cad: " + cad);
+
+        System.out.println("Valor de PI: " + PI);
+    }
+}
